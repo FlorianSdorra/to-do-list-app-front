474c706b6e9332789798e4936196215838cd7a40
FlorianSdorra <fsd114@posteo.de>
2020-01-27 11:37:10 +0100

    cleaned up to start

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Flo</h1>
+    </div>
+  );
+}
+
+export default App;
